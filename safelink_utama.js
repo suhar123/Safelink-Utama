9dc2afccd47b459b680d0f61a4b323ee9becd5f8
suhar123 <adititik527@gmail.com>
2018-04-09 02:39:28 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,13 +19,16 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*8)+ 1);
 var xxx=null;
-if(x=="1"){xxx="what-is-bitcoin.html"}
+if(x=="1"){xxx="fabulous-and-easy-tips-for-skin.html"}
 if(x=="2"){xxx="powerful-meditation-methods.html"}
 if(x=="3"){xxx="the-best-tips-for-skin-care.html"}
 if(x=="4"){xxx="what-does-body-need-to-stay-healthy.html"}
-if(x=="5"){xxx="natural-beauty-to-add-shine-to-your-hair.html"}a_to_ve[i].href="https://your-link1.blogspot.co.id/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="importance-of-spa-to-improve-beauty.html"}
+if(x=="6"){xxx="10-beauty-hacks-for-beautiful-skin.html"}
+if(x=="7"){xxx="what-is-beauty-to-you.html"}
+if(x=="8"){xxx="natural-beauty-to-add-shine-to-your-hair.html"}a_to_ve[i].href="https://your-link1.blogspot.co.id/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
